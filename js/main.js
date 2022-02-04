dd63f7cd7c16f1cb4e327290153c90dc9b30115a
Tomasz <stonka123@gmail.com>
2022-02-04 17:20:39 +0100

    section aboutus, header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const burgerBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav')
 const navItems = document.querySelectorAll('.nav__items')
+const navBar = document.querySelector('.nav__bar')
 
 const showNav = () => {
 	burgerBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav__mobile--active')
 }
-
+// const handleObserve = () => {
+// 	if (window.scrollY > '640') {
+// 		navBar.classList.toggle('bars')
+// 		// console.log('lol')
+// 	}
+// }
+// handleObserve()
 burgerBtn.addEventListener('click', showNav)
