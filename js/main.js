841a3f3e456efcf3dbdbb1b91cf6a87471c10b70
Tomasz <stonka123@gmail.com>
2022-02-09 11:51:55 +0100

    responsive now

--- js/main.js
+++ js/main.js
@@ -2,18 +2,42 @@ const burgerBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav')
 const navItems = document.querySelectorAll('.nav__items')
 const navBar = document.querySelector('.nav__bar')
+const time = document.getElementById('myTime')
+const footer = document.querySelector('.footer__text--year')
+const main = document.querySelector('.main')
 
-// get new DAte
 document.getElementById('today').value = new Date().toISOString().substring(0, 10)
+
 const showNav = () => {
 	burgerBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav__mobile--active')
 }
-// const handleObserve = () => {
-// 	if (window.scrollY > '640') {
-// 		navBar.classList.toggle('bars')
-// 		// console.log('lol')
+// dziala na scrollY! \/
+// const handeObserve = () => {
+// 	const currentY = window.scrollY
+// 	if (currentY > 514) {
+// 		navBar.classList.add('nav__bar--gray')
+// 	} else {
+// 		navBar.classList.remove('nav__bar--gray')
 // 	}
 // }
-// handleObserve()
+
+const handleObserve = () => {
+	const currentY = window.scrollY
+	if (main.classList.contains('main') && main.offsetTop <= currentY + 40) {
+		navBar.classList.add('nav__bar--gray')
+	} else {
+		navBar.classList.remove('nav__bar--gray')
+	}
+}
+
+// footer
+
+const checkYear = () => {
+	const year = new Date().getFullYear()
+	footer.textContent = year
+}
+checkYear()
+
 burgerBtn.addEventListener('click', showNav)
+window.addEventListener('scroll', handleObserve)
