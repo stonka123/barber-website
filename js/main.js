af29cadf91266374b2975d9842fab38686f56980
Tomasz <stonka123@gmail.com>
2022-02-10 10:47:26 +0100

    responsive

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
 const burgerBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav')
 const navItems = document.querySelectorAll('.nav__items')
+const navItem = document.querySelectorAll('.nav__item')
 const navBar = document.querySelector('.nav__bar')
 const time = document.getElementById('myTime')
 const footer = document.querySelector('.footer__text--year')
 const main = document.querySelector('.main')
-
-
 
 document.getElementById('today').value = new Date().toISOString().substring(0, 10)
 
 const showNav = () => {
 	burgerBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav__mobile--active')
+
+	navItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav__mobile--active')
+			burgerBtn.classList.remove('is-active')
+		})
+	})
+	handleNavItemsAnimation()
 }
-// dziala na scrollY! \/
-// const handeObserve = () => {
-// 	const currentY = window.scrollY
-// 	if (currentY > 514) {
-// 		navBar.classList.add('nav__bar--gray')
-// 	} else {
-// 		navBar.classList.remove('nav__bar--gray')
-// 	}
-// }
 
 const handleObserve = () => {
 	const currentY = window.scrollY
@@ -32,7 +30,14 @@ const handleObserve = () => {
 		navBar.classList.remove('nav__bar--gray')
 	}
 }
-
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+	navItem.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
 // footer
 
 const checkYear = () => {
--- js/slick.js
+++ js/slick.js
@@ -1,27 +1,18 @@
 // $(document).ready(function () {
 // 	$('.price').slick({
-// 		centerMode: true,
 // 		mobileFirst: true,
 // 		adaptiveHeight: true,
 // 		centerPadding: '2px',
 // 		slidesToShow: 1,
+// 		arrows: false,
 // 		responsive: [
-// 			{
-// 				breakpoint: 768,
-// 				settings: {
-// 					arrows: false,
-// 					centerMode: true,
-// 					centerPadding: '40px',
-// 					slidesToShow: 3,
-// 				},
-// 			},
 // 			{
 // 				breakpoint: 319,
 // 				settings: {
-// 					accessibility: false,
+// 					accessibility: true,
 // 					adaptiveHeight: true,
 // 					arrows: false,
-// 					centerMode: false,
+// 					centerMode: true,
 // 					// centerPadding: '4px',
 // 					slidesToShow: 1,
 // 				},
